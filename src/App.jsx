aef2dc3ef3325e7952cd721c6b97547ddd853a3b
sagar <sagarramrudri@gmail.com>
2024-11-10 08:01:40 +0545

    top info is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 
 import Contact from "./Component/Contact"
 import ExpSkillProjSection from "./Component/ExpSkillProjSection"
-import TopInfo from "./Component/TopInfo"
+import TopInfo from "./Component/home/TopInfo"
 
 function App() {
 
 
   return (
     <div className="max-w-screen-xl mx-[auto]">
-      <TopInfo />
-      <ExpSkillProjSection />
-      <Contact />
+      <TopInfo/>
     </div>
   )
 }
---
+++ src/Component/home/PersonalInfo.jsx
@@ -0,0 +1,15 @@
+import profileImage from "C:/Users/sagar/Desktop/Sarang/Sagar/src/assets/sagarprofile.jpeg"
+export default function () {
+    return (
+        <div className="bg-[#171717] overflow-hidden max-w-screen-lg h-56 rounded-3xl flex justify-center items-center  hover:border-2 border-emerald-900 ">
+            <div className="text-slate-50 flex">
+                <div className="h-56 w-60 rounded-e-[100px] bg-cover bg-center " style={{backgroundImage: `url(${profileImage})`}}></div>
+                <div className="mx-8 pt-5">
+                    <h1 className="text-4xl font-bold mb-1">Sagar Ramrudri</h1>
+                    <h3 className="mb-2 text-base"><span className="text-xl font-semibold">MERN</span> Stack Developer</h3>
+                    <p className="text-base">Targeting MERN Developer roles with an organization of high repute with a scope of improving knowledge and further career growth.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
